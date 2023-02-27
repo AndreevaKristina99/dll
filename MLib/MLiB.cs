f26bb844ec4b4c8f2a4b41433dbee187a9f73768
Кристина Андреева <kandreeva952@gmail.com>
2023-02-28 00:58:01 +0300

    Добавьте файлы проекта.

---
+++ MLib/MLiB.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+
+namespace MLib
+{
+    public class MLiB
+    {
+        public static void MailGo(string komy,string tema, string sms)
+        {
+            SmtpClient Client = new SmtpClient();//создаем клиента для входы в свою почту
+            Client.Credentials = new NetworkCredential("адрес@yandex.ru", "пароль");
+            Client.Host = "smtp.yandex.ru";
+            Client.Port = 587;//порт использующий домен яндекса
+            Client.EnableSsl = true;//запрос на проверку надежности входа
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("andreeva.cr1stin@yandex.ru");//от кого 
+            mail.To.Add(new MailAddress(komy));//кому отправляется письмо
+            mail.Subject =tema;
+            mail.IsBodyHtml = true;//для html теней
+            mail.Body =sms;//текст сообщения
+            Client.Send(mail);
+        }
+    }
+}
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MLib;
+
+
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+           
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string komy = textBox1.Text;
+            string tema = textBox2.Text;
+            string sms=textBox3.Text;
+
+             MLiB lib = new MLiB();
+            lib.MailGo(komy,tema,sms);
+
+        }
+    
+    }
+}
